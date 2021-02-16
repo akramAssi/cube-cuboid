5f74cf10ab12badbdb65395898a8bb9ff53d5c4b
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-02-16 18:36:33 +0200

    add setter and getter

--- src/cubeandcuboid_akramassi_home2/Cuboid.java
+++ src/cubeandcuboid_akramassi_home2/Cuboid.java
@@ -9,6 +9,48 @@ package cubeandcuboid_akramassi_home2;
  *
  * @author Assi_workstation
  */
-public class Cuboid {
-    private 
+public class  Cuboid <type>{
+    private type length;
+    private type width;
+    private type height;
+
+    public Cuboid(type length, type width, type height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    public type getLength() {
+        return length;
+    }
+
+    public void setLength(type length) {
+        this.length = length;
+    }
+
+    public type getWidth() {
+        return width;
+    }
+
+    public void setWidth(type width) {
+        this.width = width;
+    }
+
+    public type getHeight() {
+        return height;
+    }
+
+    public void setHeight(type height) {
+        this.height = height;
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return "Cuboid{" + "length=" + length + ", width=" + width + ", height=" + height + '}';
+    }
+    
+    
+  
 }
