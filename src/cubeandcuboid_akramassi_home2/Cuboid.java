2dacaacd2283b4e05be3329720a66faa5165d3bf
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-02-16 18:27:03 +0200

    initiate

---
+++ src/cubeandcuboid_akramassi_home2/Cuboid.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cubeandcuboid_akramassi_home2;
+
+/**
+ *
+ * @author Assi_workstation
+ */
+public class Cuboid {
+    private 
+}
